76d179683edde2ca71c3fa18541092f040984653
Akki07-code <112249951+Akki07-code@users.noreply.github.com>
2023-06-14 21:45:01 +0530

    Create majorityelement.java
    In the given input, the majority element is 5 because it appears more than n/2 times in the array, where n is the size of the array. The Java code provided uses the same Boyer-Moore Majority Vote algorithm as mentioned earlier in the Python implementation.

---
+++ majorityelement.java
@@ -0,0 +1,37 @@
+public class MajorityElement {
+    public static int findMajorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify if the candidate is the majority element
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > nums.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 5, 5, 5, 5, 5};
+        int result = findMajorityElement(nums);
+        System.out.println(result); // Output: 5
+    }
+}
